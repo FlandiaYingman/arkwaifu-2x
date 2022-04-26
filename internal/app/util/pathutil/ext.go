6d673910f65fca757d53a2a5a099940f045ab6a3
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-04-26 21:00:45 +0800

    feat(arkwaifu-2x): create packages `fileutil` and `pathutil`

--- internal/app/util/pathutil/ext.go
+++ internal/app/util/pathutil/ext.go
@@ -6,7 +6,33 @@ import (
 	"strings"
 )
 
+// RemoveExt returns the path without the extension.
+func RemoveExt(path string) string {
+	return strings.TrimSuffix(path, filepath.Ext(path))
+}
+
+// RemoveAllExt returns the path without all extensions.
+func RemoveAllExt(path string) string {
+	return strings.TrimSuffix(path, AllExt(path))
+}
+
+func AllExt(path string) string {
+	_, after, found := strings.Cut(path, ".")
+	if found {
+		return "." + after
+	} else {
+		return ""
+	}
+}
+
+// ReplaceExt replaces the extension of the given path with the given extension.
 func ReplaceExt(path string, ext string) string {
-	withoutExt := strings.TrimSuffix(path, filepath.Ext(path))
+	withoutExt := RemoveExt(path)
+	return fmt.Sprintf("%v%v", withoutExt, ext)
+}
+
+// ReplaceAllExt replaces all extensions of the given path with the given extension.
+func ReplaceAllExt(path string, ext string) string {
+	withoutExt := RemoveAllExt(path)
 	return fmt.Sprintf("%v%v", withoutExt, ext)
 }

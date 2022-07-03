32dd183a1787942e8cd5a6db0ce2ff4db6d52798
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-07-03 15:42:21 +0800

    feat(arkwaifu-2x): prefer to upscale images and backgrounds assets first, then others

--- internal/app/fetch.go
+++ internal/app/fetch.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/flandiayingman/arkwaifu-2x/internal/app/api"
 	"github.com/flandiayingman/arkwaifu-2x/internal/app/dto"
+	"github.com/samber/lo"
 	"go.uber.org/zap"
 )
 
@@ -28,12 +29,26 @@ func CreateTask(dstDir *string) *FetchTask {
 		)
 		return nil
 	}
+	if len(upscalableAssets) == 0 {
+		zap.S().Infof("No upscalable assets found, remaining/total: %d/%d", len(upscalableAssets), len(assets))
+		return nil
+	}
+
+	preference := func(a dto.Asset) bool { return a.Kind == "images" || a.Kind == "backgrounds" }
+	upAssetsPrefers := lo.Filter(upscalableAssets, func(a dto.Asset, _ int) bool { return preference(a) })
+	upAssetsOthers := lo.Filter(upscalableAssets, func(a dto.Asset, _ int) bool { return !preference(a) })
+
 	// Select asset randomly to avoid 'race' to some degree.
 	// The 'race' refers to, like, multiple clients are upscaling the same asset,
 	// though at last they will get the same result, it's just a waste of time.
-	a := upscalableAssets[rand.Intn(len(upscalableAssets))]
+	var a dto.Asset
+	if len(upAssetsPrefers) > 0 {
+		a = upAssetsPrefers[rand.Intn(len(upAssetsPrefers))]
+	} else {
+		a = upAssetsOthers[rand.Intn(len(upAssetsOthers))]
+	}
 	t := &FetchTask{asset: a, dstDir: dstDir}
-	zap.S().Infof("Processing the asset %s, remaning/total: %d/%d", a, len(upscalableAssets), len(assets))
+	zap.S().Infof("Processing the asset %s, remaining/total: %d/%d", a, len(upscalableAssets), len(assets))
 
 	err = api.GetVariants(t.asset, *t.dstDir)
 	if err != nil {
